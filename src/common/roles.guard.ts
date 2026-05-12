a5f900d8cc0c6adc68a77c4eff647a5c28727a72
Евгений Стасенко <evgenijstasenko@MacBook-Pro-Evgenij-2.local>
2026-05-12 14:54:35 +0300

    wiow

--- src/common/roles.guard.ts
+++ src/common/roles.guard.ts
@@ -9,10 +9,10 @@ export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
 
     if (!requiredRoles || requiredRoles.length === 0) return true;
 
